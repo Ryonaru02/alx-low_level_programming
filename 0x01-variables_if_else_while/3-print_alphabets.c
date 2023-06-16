24489cc4d4596e0dc8cfcbc81c2e32014403f906
abdessamad ziani <abdessamad.ziani.0420@gmail.com>
2023-06-16 14:48:13 -0700

    up to task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i=0;
+
+	while( i < 52)
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
